4300f25aec1043bb4b858ded9615af3abe04a425
Mohan <mohanavel15@protonmail.com>
2024-06-16 20:23:24 +0530

    added llama3 and mixtral 8x7bOC

--- constants.go
+++ constants.go
@@ -6,5 +6,7 @@ const (
 	ENDPOINT_STATUS = ENDPOINT + "/status"
 	GPT3dot5Turbo   = "gpt-3.5-turbo-0125"
 	Claude3Haiku    = "claude-3-haiku-20240307"
+	Llama3v70B      = "meta-llama/Llama-3-70b-chat-hf"
+	Mixtral8x7B     = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 	USER_AGENT      = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3"
 )
